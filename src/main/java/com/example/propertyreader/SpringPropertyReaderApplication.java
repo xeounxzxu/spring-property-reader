7224cd8c402c5e4541e398509bd6526d7eeee61e
xeounxzxu <pnw123456@icloud.com>
2025-01-26 12:53:15 +0900

    first commit

---
+++ src/main/java/com/example/propertyreader/SpringPropertyReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.propertyreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertyReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPropertyReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/propertyreader/SpringPropertyReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propertyreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertyReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
